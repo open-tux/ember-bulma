42cde2851cd5cc2f29ce98a33c40cb90c918021d
Carlos Rodriguez <crodriguez1a@gmail.com>
2016-03-24 06:30:01 -0400

    remove hard dependency on sort-filter-table

--- addon/components/bulma-table.js
+++ addon/components/bulma-table.js
@@ -1,7 +1,9 @@
-import SortFilterTable from 'ember-sort-filter-table/components/sort-filter-table';
+import Ember from 'ember';
+import layout from '../templates/components/bulma-button';
 
-export default SortFilterTable.extend({
-  layoutName: 'components/sort-filter-table',
+export default Ember.Component.extend({
+  layout,
+  tagName: 'table',
   classNames: ['table'],
   classNameBindings: ['isBordered:is-bordered', 'bordered:is-bordered', 'isStriped:is-striped', 'striped:is-striped', 'isNarrow:is-narrow', 'narrow:is-narrow']
 });

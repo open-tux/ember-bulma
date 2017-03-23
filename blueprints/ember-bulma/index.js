130ff618c2396be6cee5ca5f681df717770f4e36
Alex DiLiberto <alex@alexdiliberto.com>
2017-03-23 08:55:23 -0400

    Better automation for app installation process (#52)
    @alexdiliberto Looks good man.

--- blueprints/ember-bulma/index.js
+++ blueprints/ember-bulma/index.js
@@ -2,17 +2,27 @@
 module.exports = {
   description: 'Installs Bower dependencies',
 
- 	normalizeEntityName: function() {
- 		// this prevents an error when the entityName is
- 		// not specified (since that doesn't actually matter
- 		// to us
- 	},
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
- 	afterInstall: function() {
-    return this.addBowerPackagesToProject(
+  afterInstall: function() {
+    return this.addPackagesToProject(
       [
-        { name: 'bulma', target: '0.1.2' }
+        { name: 'ember-cli-sass', target: '6.1.2' }
       ]
-    );
+    ).then(() => {
+      return this.addBowerPackagesToProject(
+        [
+          { name: 'bulma', target: '0.1.2' }
+        ]
+      );
+    });
+  },
+
+  isDevelopingAddon: function() {
+    return false;
   }
 };

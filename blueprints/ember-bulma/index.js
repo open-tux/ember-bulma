b285cf5da4696dd324bffd7711039afe71bf1d42
Jacob LeGrone <git@jacoblegrone.com>
2017-08-04 13:51:46 -0400

    feat: upgrade to bulma@0.5.0

--- blueprints/ember-bulma/index.js
+++ blueprints/ember-bulma/index.js
@@ -2,18 +2,18 @@
 module.exports = {
   description: 'Installs Bower and NPM dependencies',
 
-  normalizeEntityName: function() {
+  normalizeEntityName() {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
   },
 
-  afterInstall: function() {
+  afterInstall() {
     // NOTE: Currently adding `eslint-plugin-ember-suave` to the consuming app/addon to bypass the following error. This shouldn't be a necessary step:
     // Error: Failed to load plugin ember-suave: Cannot find module 'eslint-plugin-ember-suave'
     return this.addPackagesToProject(
       [
-        { name: 'bulma', target: '0.1.2' },
+        { name: 'bulma', target: '^0.5.0' },
         { name: 'ember-cli-sass', target: '~6.1.2' },
         { name: 'eslint-plugin-ember-suave', target: '~1.0.0' }
       ]

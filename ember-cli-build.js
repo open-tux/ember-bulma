open-tux/ember-bulma

commit 12c1c89a9561bfe52525a20bea39ec556354748c

Ember CLI v2.12 -> v2.13 (#62)

.travis.yml
@@ -6,9 +6,7 @@ node_js:
 sudo: false
 
 cache:
-  directories:
-    - $HOME/.npm
-    - $HOME/.cache # includes bowers cache
+  yarn: true
 
 env:
   # we recommend testing LTS's and latest stable release (bonus points to beta/canary)
@@ -25,13 +23,14 @@ matrix:
     - env: EMBER_TRY_SCENARIO=ember-canary
 
 before_install:
-  - npm config set spin false
-  - npm install -g bower phantomjs-prebuilt
+  - curl -o- -L https://yarnpkg.com/install.sh | bash
+  - export PATH=$HOME/.yarn/bin:$PATH
+  - yarn global add bower phantomjs-prebuilt
   - bower --version
   - phantomjs --version
 
 install:
-  - npm install
+  - yarn install --no-lockfile
   - bower install
 
 script:

ember-cli-build.js
@@ -5,7 +5,7 @@ module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     // Add options here
     babel: {
-      optional: ['es7.decorators']
+      plugins: ['transform-decorators-legacy']
     }
   });
 

tests/dummy/app/app.js
@@ -9,8 +9,6 @@ const {
 
 let App;
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
 App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,

.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 7,
+    ecmaVersion: 2017,
     sourceType: 'module'
   },
   extends: [

.template-lintrc.js
@@ -1,4 +1,4 @@
-/* jshint node:true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {

LICENSE
@@ -1,21 +1,0 @@
-The MIT License (MIT)
-
-Copyright (c) 2016 open-tux
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.

index.js
@@ -1,27 +1,27 @@
 /* eslint-env node */
 'use strict';
 
-var path = require('path');
-var Funnel = require('broccoli-funnel');
-var intersection = require('./lib/intersection');
-var difference = require('./lib/difference');
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const intersection = require('./lib/intersection');
+const difference = require('./lib/difference');
 
 module.exports = {
   name: 'ember-bulma',
 
-  init: function(app) {
+  init(app) {
     this._super.init && this._super.init.apply(this, arguments);
 
     this.options = this.options || {};
     this.options.babel = this.options.babel || {};
-    this.options.babel.optional = this.options.babel.optional || [];
+    this.options.babel.plugins = this.options.babel.plugins || [];
 
-    if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
-      this.options.babel.optional.push('es7.decorators');
+    if (this.options.babel.plugins.indexOf('transform-decorators-legacy') === -1) {
+      this.options.babel.plugins.push('transform-decorators-legacy');
     }
   },
 
-  included: function(app, parentAddon) {
+  included(app, parentAddon) {
     this._super.included.apply(this, arguments);
 
     var target = parentAddon || app;
@@ -56,14 +56,14 @@ module.exports = {
     https://github.com/DockYard/ember-composable-components/
     https://github.com/DockYard/ember-composable-components#configuration
   */
-  treeForAddon: function() {
+  treeForAddon() {
     // see: https://github.com/ember-cli/ember-cli/issues/4463
     var tree = this._super.treeForAddon.apply(this, arguments);
 
     return this.filterComponents(tree, new RegExp('^modules\/' + this.name + '\/components\/', 'i'));
   },
 
-  filterComponents: function(tree, regex) {
+  filterComponents(tree, regex) {
     var whitelist = this.whitelist;
     var blacklist = this.blacklist;
     var _this = this;
@@ -85,7 +85,7 @@ module.exports = {
     return funnelTree;
   },
 
-  exclusionFilter: function(name, regex, lists) {
+  exclusionFilter(name, regex, lists) {
     var whitelist = lists.whitelist || [];
     var blacklist = lists.blacklist || [];
     var isAddonComponent = regex.test(name);
@@ -121,7 +121,7 @@ module.exports = {
     return !isWhitelisted || isBlacklisted;
   },
 
-  generateWhitelist: function(addonConfig) {
+  generateWhitelist(addonConfig) {
     var only = addonConfig.only || [];
     var except = addonConfig.except || [];
 
@@ -132,7 +132,7 @@ module.exports = {
     return only;
   },
 
-  generateBlacklist: function(addonConfig) {
+  generateBlacklist(addonConfig) {
     var only = addonConfig.only || [];
     var except = addonConfig.except || [];
 
@@ -143,7 +143,7 @@ module.exports = {
     return except;
   },
 
-  isDevelopingAddon: function() {
+  isDevelopingAddon() {
     return true;
   }
 };

tests/dummy/config/targets.js
@@ -1,0 +1,10 @@
+/* eslint-env node */
+
+module.exports = {
+  browsers: [
+    'ie 9',
+    'last 1 Chrome versions',
+    'last 1 Firefox versions',
+    'last 1 Safari versions'
+  ]
+};

commit 0fcda3fc77d18fea4b35d2410048d404ed50d4f6

fix(cli): update to latest cli
fix an issue introduced in https://github.com/open-tux/ember-bulma/commit/12c1c89a9561bfe52525a20bea39ec556354748c. Previously, when deploying the dummy app (prod build) or serving the app locally with prod (e s -prod), you would get a run-time error in vendor.js. This fixes the bug.

.travis.yml
@@ -15,14 +15,18 @@ cache:
   yarn: true
 
 env:
-  # we recommend new addons test the current and previous LTS
-  # as well as latest stable release (bonus points to beta/canary)
-  - EMBER_TRY_SCENARIO=ember-lts-2.8
-  - EMBER_TRY_SCENARIO=ember-lts-2.12
-  - EMBER_TRY_SCENARIO=ember-release
-  - EMBER_TRY_SCENARIO=ember-beta
-  - EMBER_TRY_SCENARIO=ember-canary
-  - EMBER_TRY_SCENARIO=ember-default
+  global:
+    # See https://git.io/vdao3 for details.
+    - JOBS=1
+  matrix:
+    # we recommend new addons test the current and previous LTS
+    # as well as latest stable release (bonus points to beta/canary)
+    - EMBER_TRY_SCENARIO=ember-lts-2.8
+    - EMBER_TRY_SCENARIO=ember-lts-2.12
+    - EMBER_TRY_SCENARIO=ember-release
+    - EMBER_TRY_SCENARIO=ember-beta
+    - EMBER_TRY_SCENARIO=ember-canary
+    - EMBER_TRY_SCENARIO=ember-default
 
 matrix:
   fast_finish: true

ember-cli-build.js
@@ -6,9 +6,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
-    babel: {
-      plugins: ['transform-decorators-legacy']
-    }
   });
 
   /*

tests/dummy/app/app.js
@@ -1,9 +1,9 @@
-import Ember from 'ember';
+import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const App = Ember.Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver

tests/dummy/app/index.html
@@ -9,8 +9,8 @@
 
     {{content-for "head"}}
 
-    <link rel="stylesheet" href="{{rootURL}}assets/vendor.css">
-    <link rel="stylesheet" href="{{rootURL}}assets/dummy.css">
+    <link integrity="" rel="stylesheet" href="{{rootURL}}assets/vendor.css">
+    <link integrity="" rel="stylesheet" href="{{rootURL}}assets/dummy.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     {{content-for "head-footer"}}

tests/dummy/app/router.js
@@ -1,9 +1,5 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
-
-const {
-  Router: EmberRouter
-} = Ember;
 
 const Router = EmberRouter.extend({
   location: config.locationType,

tests/dummy/config/environment.js
@@ -55,6 +55,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    // here you can enable a production-specific feature
     ENV.rootURL = '/ember-bulma';
     ENV.locationType = 'hash';
   }

tests/helpers/destroy-app.js
@@ -1,8 +1,4 @@
-import Ember from 'ember';
-
-const {
-  run
-} = Ember;
+import { run } from '@ember/runloop';
 
 export default function destroyApp(application) {
   run(application, 'destroy');

tests/helpers/module-for-acceptance.js
@@ -1,9 +1,7 @@
 import { module } from 'qunit';
-import Ember from 'ember';
+import { resolve } from 'rsvp';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
-
-const { RSVP: { resolve } } = Ember;
 
 export default function(name, options = {}) {
   module(name, {

tests/helpers/start-app.js
@@ -1,12 +1,13 @@
-import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
+import { merge } from '@ember/polyfills';
+import { run } from '@ember/runloop';
 
 export default function startApp(attrs) {
-  let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+  let attributes = merge({}, config.APP);
+  attributes = merge(attributes, attrs); // use defaults, but you can override;
 
-  return Ember.run(() => {
+  return run(() => {
     let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();